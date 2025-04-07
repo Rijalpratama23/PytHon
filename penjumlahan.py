c4023e8f9cb0bbaa092ba4511d516fdb874659a8
RijalPratama21 <rijal.pratama_ti23@nusaputra.ac.id>
2025-04-07 08:30:37 +0700

    menambahkan program penjumlahan sederhana

---
+++ penjumlahan.py
@@ -0,0 +1,8 @@
+a = int(input("Masukan angka Pertama = "))
+b = int(input("Masukan angka kedua = "))
+
+result = a + b
+
+
+print('hasil penjumlahannya adalah angka',a, 'dan angka', b, 'adalah ',result);
+
